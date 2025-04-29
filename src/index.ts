9c1d62c508d6017cd9e78335779d11c55ec50b3f
Andréas B <andreas.blondeau@gmail.com>
2025-04-29 22:37:00 +0200

    setup firecrawl

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World")
+import FirecrawlApp, { CrawlParams, CrawlStatusResponse } from '@mendable/firecrawl-js';
+import { ChatOpenAI } from "@langchain/openai";
+
+const firecrawl = new FirecrawlApp({
+  apiKey: process.env.FIRECRAWL_API_KEY,
+});
+
+// Scrape a website:
+const scrapeResult = await firecrawl.scrapeUrl('reelsmaker.ai', { formats: ['markdown', 'html'] })
+
+if (!scrapeResult.success) {
+  throw new Error(`Failed to scrape: ${scrapeResult.error}`)
+}
+
+console.log(scrapeResult)
